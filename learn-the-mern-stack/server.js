05834351384e08e4d8193a284e5b7837c9f35c7d
bmoishe <moishebarnes@gmail.com>
2018-11-24 15:21:51 +0000

    connected DB

---
+++ learn-the-mern-stack/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.json());
+// DB config. this is getting hidden variable from my config file
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started on port '+ port));
