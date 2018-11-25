b6a8013cf86c4cbbc8fb98ec0271604e0c1d7dc8
bmoishe <moishebarnes@gmail.com>
2018-11-25 18:12:06 +0000

    Front end complete

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+// Add an action type fro anything you will do in the applications
